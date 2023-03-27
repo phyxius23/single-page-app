eb106bb428390ae11b8ed225259fc3a3776dcb0f
phyxius23 <30ntin@gmail.com>
2023-03-27 17:25:08 +0200

    update css and tag img

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ButtonComponent from './components/ButtonComponent';
 import ImageComponent from './components/ImageComponent';
+import imageLabrador from './image2.jpg';
 
 function App() {
   return (
@@ -19,8 +20,17 @@ function App() {
         >
           Learn React
         </a>
-        <ImageComponent src="https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" alt="immagine random" />
-      <ButtonComponent text="Testo del bottone"/>
+
+        {/* sotto carico una immagine via cdn */}
+        <ImageComponent src="https://images.unsplash.com/photo-1678780593184-c71d50923fd9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80" alt="immagine random" />
+
+        {/* sotto carico una immagine via file che si trova all'interno di assets/img */}
+        <ImageComponent src="./assets/img/image1.jpg" alt="immagine random" />
+
+        {/* sotto carico una immagine tramite import */}
+        <ImageComponent src={imageLabrador} />
+
+        <ButtonComponent text="Testo del bottone"/>
       </header>
     </div>
   );
